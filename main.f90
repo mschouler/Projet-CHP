6b82f43cbf1d4ac06f64fb9f552a71cb63359a86
mschouler <schouler.marc@gmail.com>
2016-04-06 18:14:25 +0200

    Add files

--- main.f90
+++ main.f90
@@ -1,29 +1,15 @@
 program projet
 use proj_1
 implicit none
-real :: Lx, Ly, dx, dy
+real :: Lx, Ly, dx, dy, dt, D
+real*8::alpha, beta, gama
 integer :: Nx, Ny, i, j
-real, dimension(:), allocatable :: U, F, X, Y;
+real*8, dimension(:), allocatable :: U, Test, Uexacte, F, X, Y;
 real*8,dimension(4,4)::A
 real*8,dimension(4)::b
 real*8,dimension(4)::X2
-real*8,dimension(20)::X1,C
-!X2=0.
-!print*,X2
-!b=1.
-!A(1,:)=(/10.,-1.,-1.,-1./)
-!A(2,:)=(/-1.,10.,-1.,-1./)
-!A(3,:)=(/-1.,-1.,10.,-1./)
-!A(4,:)=(/-1.,-1.,-1.,10./)
+real*8,dimension(:),allocatable::X1, C
 
-!call grad_conj(4,A,X2,b)
-!print*,matmul(A,X2)
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!! TEST DE LA BOUCLE MULT !!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 Lx=1
 Ly=1
@@ -37,7 +23,11 @@ dx = Lx/Nx
 dy = Ly/Ny
 
 allocate(U(1:Nx*Ny))
+allocate(Test(1:Nx*Ny))
+allocate(Uexacte(1:Nx*Ny))
 allocate(F(1:Nx*Ny))
+allocate(X1(Nx*Ny))
+allocate(C(Nx*Ny))
 
 allocate(X(1:Nx))
 allocate(Y(1:Ny))
@@ -52,14 +42,56 @@ enddo
 
 do i=1,Ny
  do j=1,Nx
-    F((i-1)*Nx+j) = Y(j) - Y(j)**2 + X(i) - X(i)**2
+    F((i-1)*Nx+j) = Y(i) - Y(i)**2 + X(j) - X(j)**2
+    Uexacte((i-1)*Nx+j)=X(j)*(1-X(j))*Y(i)*(1-Y(i))
  end do
 end do
 
-write(*,*)F
+open( unit=10, &
+file = "f.txt", &
+action = "write", &
+status = "unknown")
+do i=1,Ny
+    do j=1,Nx
+    write(10,*) Y(i),X(j),F((i-1)*Nx+j)
+    end do
+    write(10,*) " "
+end do
+close(10)
 
-X1=1.
-call mult(1.0D0,1.0D0,1.0D0,X1,C,Nx,Ny)
+U=1.
+D=1.
+dt=1.
+alpha=1./dt + 2.*D/dx**2 + 2.*D/dy**2
+beta=-D/dx**2.
+gama=-D/dy**2.
+call grad_conj(Nx, Ny, alpha, beta, gama, U, F)  
+!!$call mult(1.D0, 1.D0, 1.D0, U, Test, Nx, Ny)
+!!$print*, Test
+
+open( unit=12, &
+file = "sol.txt", &
+action = "write", &
+status = "unknown")
+do i=1,Ny
+    do j=1,Nx
+    write(12,*) Y(i),X(j),U((i-1)*Nx+j)
+    end do
+    write(12,*) " "
+end do
+close(12)
+
+!!$open( unit=13, &
+!!$file = "solexacte.txt", &
+!!$action = "write", &
+!!$status = "unknown")
+!!$do i=1,Ny
+!!$    do j=1,Nx       
+!!$    write(13,*) Y(i),X(j),Uexacte((i-1)*Nx+j)
+!!$    end do
+!!$    write(13,*) " "
+!!$end do
+!!$close(13)
 
 
 end program
--- proj_1.f90
+++ proj_1.f90
@@ -3,36 +3,36 @@ implicit none
 
 
 contains
-  subroutine grad_conj(n,A,X,b)
+  subroutine grad_conj(Nx, Ny, alpha, beta, gama, X, b)
     implicit none
-    integer,intent(in)::n
-    real*8,intent(in),dimension(n,n)::A
-    real*8,intent(inout),dimension(n)::X
-    real*8,intent(in),dimension(n)::b
-    real*8,dimension(n)::w,d,R,R1
-    integer::l=0
+    integer,intent(in)::Nx, Ny
+    real*8,intent(inout),dimension(Nx*Ny)::X
+    real*8,intent(in),dimension(Nx*Ny)::b
+    real*8,intent(in)::alpha,beta,gama
+    real*8,dimension(Nx*Ny)::w,d,R,R1
+    integer::l=0, n
     integer,parameter::niter=10000
-    real*8,parameter::eps=1e-5
-    real*8::beta,alpha
+    real*8,parameter::eps=1e-3
+    real*8::beta_,alpha_
+    n=Nx*Ny
    
     !initialisation 
-    R=matmul(A,X)-b
+    call mult(alpha, beta, gama, X, R, Nx, Ny)
+    R = R - b
     d=R
    
     !boucle 
     do while(norme(R,n)>eps .and. l<=niter)
-       print*,'iteration',l
-       w=matmul(A,d)
-       print*,w
-       alpha=scal(d,R,n)/scal(d,w,n)
-       X=X-alpha*d
-       R1=R-alpha*w
-       beta=norme(R1,n)**2/norme(R,n)**2
-       d=R1+beta*d
+       call mult(alpha, beta, gama, d, w, Nx, Ny)
+       alpha_=scal(d,R,n)/scal(d,w,n)
+       X=X-alpha_*d
+       R1=R-alpha_*w
+       beta_=norme(R1,n)**2/norme(R,n)**2
+       d=R1+beta_*d
        R=R1
        l=l+1
     end do
-    
+    print*,'iteration',l
     end subroutine
 
     function norme(X,n)
@@ -67,11 +67,15 @@ contains
           real*8, dimension(Nx*Ny), intent(in) :: X
           real*8, dimension(Nx*Ny), intent(out) :: B
           n=Nx*Ny
-
-          do i=1,Nx
-             B(i) = alpha*X(i) + beta*X(i+1) + gama*X(i+Nx)
-          enddo
           
+          B(1)=alpha*X(1) + beta*X(2) + gama*X(1+Nx)
+
+          do i=2,Nx-1
+             B(i) = alpha*X(i) + beta*X(i-1)+beta*X(i+1) + gama*X(i+Nx)
+          enddo
+
+          B(Nx)=alpha*X(Nx) + beta*X(Nx-1) + gama*X(2*Nx)
+
           do i=Nx+1, n-(Nx+1)
              if (MOD(i, Nx) == 1) then
                 k=0
@@ -85,13 +89,17 @@ contains
                 l=1
              endif
              
-             B(i) = gama*(X(i-Nx) + X(i+Nx)) + beta*(k*X(i-1) + l*X(i+1)) + gama*X(i)
+             B(i) = gama*(X(i-Nx) + X(i+Nx)) + beta*(k*X(i-1) + l*X(i+1)) + alpha*X(i)
           enddo
 
-          do i=n-Nx, n
+        B(n-Nx)=gama*(X(n-2*Nx) + X(n)) + alpha*X(n-Nx) + beta*X(n-Nx+1)
+
+          do i=n-Nx+1, n-1
              
-             B(i) = gama*X(i-Nx) + beta*X(i-1) + alpha*X(i)
+             B(i) = gama*X(i-Nx) + beta*X(i-1) + beta*X(i+1) + alpha*X(i)
           enddo
+
+        B(n)=gama*X(n-Nx) + beta*X(n-1) + alpha*X(n)
 
         end subroutine mult
       
