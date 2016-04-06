7588cc0b21236ee8fa81bdb484e4f72de09c90cd
jafarpenot <jafar.penot@gmail.com>
2016-04-06 19:34:24 +0200

    main

--- main.f90
+++ main.f90
@@ -4,13 +4,13 @@ implicit none
 real :: Lx, Ly, dx, dy, dt, D
 real*8::alpha, beta, gama
 integer :: Nx, Ny, i, j
-real*8, dimension(:), allocatable :: U, Test, Uexacte, F, X, Y;
+real*8, dimension(:), allocatable :: U, Uexacte,  F, X, Y;
 real*8,dimension(4,4)::A
 real*8,dimension(4)::b
 real*8,dimension(4)::X2
 real*8,dimension(:),allocatable::X1, C
 
-
+! INITIALISATION DU NOMBRE DE MAILLES ET DU PAS
 Lx=1
 Ly=1
 
@@ -22,10 +22,7 @@ read*,Ny
 dx = Lx/Nx
 dy = Ly/Ny
 
-allocate(U(1:Nx*Ny))
-allocate(Test(1:Nx*Ny))
-allocate(Uexacte(1:Nx*Ny))
-allocate(F(1:Nx*Ny))
+! INITIALISATION DES VECTEURS X  ET Y
 allocate(X1(Nx*Ny))
 allocate(C(Nx*Ny))
 
@@ -40,6 +37,13 @@ do i=1,Ny
     Y(i) = i*dy
 enddo
 
+!  INITIALISATION DE LA SOLUTOIN INITIALE ET DE LA SOLUTION EXACTE
+allocate(U(1:Nx*Ny))
+allocate(Uexacte(1:Nx*Ny))
+allocate(F(1:Nx*Ny))
+
+U=1.
+
 do i=1,Ny
  do j=1,Nx
     F((i-1)*Nx+j) = Y(i) - Y(i)**2 + X(j) - X(j)**2
@@ -47,7 +51,8 @@ do i=1,Ny
  end do
 end do
 
-open( unit=10, &
+!  ECRITURE DE LA SOLUTION INITIALE DANS UN FICHIER
+open( unit=10, &	
 file = "f.txt", &
 action = "write", &
 status = "unknown")
@@ -59,39 +64,52 @@ do i=1,Ny
 end do
 close(10)
 
+
+!  INITIALISATION DES VARIABLES DU PB
 U=1.
 D=1.
 dt=1.
 alpha=1./dt + 2.*D/dx**2 + 2.*D/dy**2
 beta=-D/dx**2.
 gama=-D/dy**2.
+
+print*, " alpha ", alpha
+print*, " beta " , beta
+print*, " gama " , gama
+
+
+
+!  ON APPELLE GRADCONJUG
 call grad_conj(Nx, Ny, alpha, beta, gama, U, F)  
 !!$call mult(1.D0, 1.D0, 1.D0, U, Test, Nx, Ny)
 !!$print*, Test
 
-open( unit=12, &
-file = "sol.txt", &
+
+! ON ECRIT LA SOLUTION DANS UN FICHIER
+!open( unit=12, &
+!file = "sol.txt", &
+!action = "write", &
+!status = "unknown")
+!do i=1,Ny
+!    do j=1,Nx
+!    write(12,*) Y(i),X(j),U((i-1)*Nx+j)
+!    end do
+ !   write(12,*) " "
+!end do
+close(12)
+
+open( unit=13, &
+file = "solexacte.txt", &
 action = "write", &
 status = "unknown")
 do i=1,Ny
     do j=1,Nx
-    write(12,*) Y(i),X(j),U((i-1)*Nx+j)
+       
+    write(13,*) Y(i),X(j),Uexacte((i-1)*Nx+j)
     end do
-    write(12,*) " "
+    write(13,*) " "
 end do
-close(12)
-
-!!$open( unit=13, &
-!!$file = "solexacte.txt", &
-!!$action = "write", &
-!!$status = "unknown")
-!!$do i=1,Ny
-!!$    do j=1,Nx       
-!!$    write(13,*) Y(i),X(j),Uexacte((i-1)*Nx+j)
-!!$    end do
-!!$    write(13,*) " "
-!!$end do
-!!$close(13)
+close(13)
 
 
 end program
